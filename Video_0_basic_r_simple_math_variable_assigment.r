e209affcf893f6317278c17d36cb5bde36eed76b
Dat T Nguyen <datn@Dats-MacBook-Pro.local>
2022-12-17 12:41:04 +0100

    video 1

---
+++ Video_0_basic_r_simple_math_variable_assigment.r
@@ -0,0 +1,69 @@
+#################################################################
+
+a = 1
+
+print(a)
+
+b = 1.2
+
+print(b)
+
+c = "Hello, chao mung cac ban den voi ngon ngu R"
+
+print(c)
+
+a
+
+b
+
+c
+
+#### 2.1 Simple math
+
+2+3
+
+14/6
+
+14/6+5
+
+14/(6+5)
+
+3^2
+
+#### 2.2 Logical comparision
+
+3^2 == 9
+
+3^2 != 9
+
+3 < 2
+
+1<=3
+
+### Some math functions
+
+#### Doing logarithms and exponentials
+
+log(x=27,base=3) #log3(9)
+
+log(27,3)
+
+exp(x=3) # e^3
+
+exp(1)
+
+#### Very large and very small number - the "e" notation
+
+1e6
+
+1*10^6
+
+#### Some other functions
+
+sqrt(9)
+
+sum(1,2,3,4)
+
+prod(1,2,3,4)
+
+#################################################################
